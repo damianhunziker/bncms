5a7a395c965f55857ba0899d6e77ecaa8583a603
Jan Marti <jmar83@users.noreply.github.com>
2019-12-15 12:21:26 +0100

    Refactoring: Logging-Einstellungen besser so!!

--- bncms/admin/start.inc.php
+++ bncms/admin/start.inc.php
@@ -1,5 +1,13 @@
 <?php
-error_reporting(E_WARNING);
+
+// Vorher:
+// error_reporting(E_WARNING);
+
+// Nachher:
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+
 
 /** @license bncms
  *
